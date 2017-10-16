d75ca5ea63be30cc9669e605b77f4385ee745b02
Mike <brayntwyster@gmail.com>
2017-10-16 09:50:36 -0400

    My last efforts on Results Page..

--- app/containers/Results/index.js
+++ app/containers/Results/index.js
@@ -1,38 +1,188 @@
 /*
  *
- * Results Page
+ * Results
  *
  */
 
-import React from 'react';
-import Helmet from 'react-helmet';
-import Navbar from 'components/Navbar';
-import SignupBox from 'components/SignupBox';
-import SignInBox from 'components/SignInBox';
-import WouldYouLike from 'components/WouldYouLike';
-import './style.css';
-import './styleM.css';
+ import React from 'react';
+ import Helmet from 'react-helmet';
+ import Navbar from 'components/Navbar';
+ import SignupBox from 'components/SignupBox';
+ import SignInBox from 'components/SignInBox';
+ import WouldYouLike from 'components/WouldYouLike';
+ import './style.css';
+ import './styleM.css';
 
 
-export default class Results extends React.PureComponent {
-  constructor(){
-    super();
-    this.state={
-      products:[]
-    }
-  }
+ export default class Results extends React.PureComponent {
+   constructor(){
+     super();
+     this.state={
+       products:[]
+     }
+   }
 
-  getResults = () => {
-    /* Not entirely sure that this is still needed.*/
-  }/*End Function*/
 
-  /*sortResultsByKey = (array, key) => {
-    return array.sort(function(a, b){
-      let x = a[key];
-      let y = b[key];
-      return((x < y) ? -1 : 0);
-    })
-  }*//*End Function*/
+   componentWillMount() {
+     this.getSearchData();
+   }
+
+   getSearchData = () => {
+     let data = new FormData();
+     let _this = this;
+     data.append('userID', 1);
+
+     fetch ('http://localhost:8000/api/collectSearchData',{
+       method: 'POST',
+       body: data
+     })
+     .then(function(response){
+       return response.json();
+     })
+     .then(function(json){
+       console.log(json.searchData);
+       _this.setState ({
+         searchData:json.searchData
+       }, function() {
+         _this.getProducts(json.searchData);
+       })
+     }.bind(this))
+   }
+
+
+   getProducts = (searchData, type = 'name', order = 'desc') => {
+     let data = new FormData();
+     searchData = searchData[0];
+
+      data.append('userID', searchData.userID);
+      data.append('minInvestment', searchData.minInvestment);
+      data.append('riskLevel', searchData.riskLevel);
+      data.append('isStock', searchData.isStock);
+      data.append('isBond', searchData.isBond);
+      data.append('isMutualFund', searchData.isMutualFund);
+      data.append('isETF', searchData.isETF);
+      data.append('isIndexFund', searchData.isIndexFund);
+      data.append('isRetirement', searchData.isRetirement);
+
+      console.log(searchData);
+
+
+     this.setState({
+       displayOptions: []
+     });
+
+     fetch ('http://localhost:8000/api/getProducts?type='+type+'&order='+order,{
+       method: 'POST',
+       body: data
+     })
+     .then(function(response){
+       return response.json();
+     })
+     .then(function(json){
+       console.log(json.resultProducts);
+       console.log(json.searchCriteria);
+       this.parseResults(json.searchCriteria);
+       this.setState({
+         getProducts:json.resultProducts,
+         message:json.message,
+         messageNum:json.messageNum
+       })
+     }.bind(this))
+   }
+
+   parseResults = (data) => {
+
+     let displayOptions = this.state.displayOptions;
+     let minInvestment = '$' + data[1];
+     let riskLevel = 'Aggressive';
+
+     if (data.length > 0) {
+       if (data[0] == 1) {
+         riskLevel = 'Aggressive';
+       }
+       else if (data[0] == 2) {
+         riskLevel = 'Moderate';
+       }
+       else if (data[0] == 3) {
+         riskLevel:'Conservative';
+       }
+     }
+     let temp = '';
+     for (let x = 2; x < data.length; x++) {
+
+         if (x < data.length - 1) {
+             temp = data[x] + ', ';
+         }
+         else {
+           temp = data[x];
+         }
+
+         displayOptions.push(temp);
+
+         this.setState({
+           displayRiskLevel:riskLevel,
+           displayOptions:displayOptions,
+           displayMinInvestment:minInvestment
+         })
+     }
+   }
+
+
+   sortResultsByKey = (array, key) => {
+     return array.sort(function(a, b){
+       let x = a[key];
+       let y = b[key];
+       return((x < y) ? -1 : 0);
+     })
+   }/*End Function*/
+
+   renderResults = () => {
+     if(this.state.messageNum !== ''){
+
+       if(this.state.messageNum == 1){
+         let options = <span>{this.state.displayOptions}</span>;
+         return (
+           <div>
+             <div>
+             Results: {this.state.getProducts.length}<br/><br/>
+               You searched on: Risk level ({this.state.displayRiskLevel}), Minimum investment ({this.state.displayMinInvestment})
+             </div>
+             <div>
+               Products: {options}<br/><br/>
+             </div>
+               {this.state.message}<br/><br/>
+               {this.state.getProducts.map((product,index)=>(
+                   <div>
+                   <div><h3>{product.name}</h3></div>
+                   <div><h4>{product.summary}</h4></div>
+                   <span>Risk level: {product.riskLevel} </span><span>Minimum investment: ${product.minInvestment} </span><span>Type of investment: {product.isStock}</span>
+                   <div><br/><br/></div>
+                   </div>
+
+               ))}
+           </div>
+         )
+       }
+       else if (this.state.messageNum == 0) {
+         console.log(this.state.messageNum + 'hiya');
+         return (
+           <div>
+           <div>
+             You searched on: Risk level ({this.state.displayRiskLevel}), Minimum investment (${this.state.searchCriteria[1]})
+           </div>
+           <div>
+             Products: {this.state.displayOptions}<br/><br/>
+           </div>
+             {this.state.message}
+           </div>
+         )
+       }
+       else {
+         return "";
+       }
+     }
+   }
+
 
 
   render() {
@@ -48,7 +198,16 @@ export default class Results extends React.PureComponent {
 
           <h1 className="openingHeader">Results Page</h1>
 
-          <div className="mobileWrapper">
+          <div className="selectWrapper">
+            <select Classname="select">
+              <option value="value1">Sort: by Fees - Low to High</option>
+              <option value="value2">Sort: by Fees - High to Low</option>
+              <option value="value3">Sort: by Performance - Low to High</option>
+              <option value="value4">Sort: by Performance - High to Low</option>
+            </select>
+          </div>
+
+          {/*}<div className="mobileWrapper">
             <div className="choicesWrapper">
               <h2 className="choicesHeader1">Sort Data By:</h2>
               <h3 className="choicesHeader2"> Fees </h3>
@@ -59,12 +218,13 @@ export default class Results extends React.PureComponent {
               </div>
             </div>{/*End className "choicesWrapper"*/}
 
-            <div className="inputWrapper">
+            {/*}<div className="inputWrapper">
 
               <h3 className="inputHeader1"></h3>
 
               <h3 className="inputHeader2">
                 <div className="content">
+                  <input type="button" onClick={() => this.getProducts(this.state.searchData, 'name', 'desc')} />
                   <input type="checkBox" onChange={this.handlePassword}/>High-Low
                   <input type="checkBox" onChange={this.handlePassword}/>Low-High
                 </div>
@@ -90,33 +250,35 @@ export default class Results extends React.PureComponent {
                   </div>
                 </h3>
               </div>{/*End className "inputWrapperSub1"*/}
-            </div>{/*End className "inputWrapper"*/}
-          </div> {/*End className "mobileWrapper"*/}
+            {/*}</div>{/*End className "inputWrapper"*/}
+          {/*}</div> {/*End className "mobileWrapper"*/}
 
-          <div className="resultsPage"><p>  Greetings! /* Currently a simple placeholder.*/</p>
+          <div className="resultsPage">
             <div className="productSummary">
-              This is where a Product Summary will be.
-            </div>/*<div className="productSummary">
-            {this.state.products.map((product, index) => (
-              <div className="listItem" key={index} >{product.name} {product.content}{product.fundPerformance}
-              </div>
-            ))}
 
-          </div>*/
+
+            {this.renderResults()}
+
+
+
+
+
+
+
+
+
+
+            </div>
+
           </div>{/*End className "resultsPage"*/}
-
-
-
-
 
         </main>
 
-
       </div>
-    );
-  }
-}
+     );
+   }
+ }
 
-Results.contextTypes = {
-  router: React.PropTypes.object
-};
+ Results.contextTypes = {
+   router: React.PropTypes.object
+ };
